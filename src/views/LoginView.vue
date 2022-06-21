e399cd3edb4794e0dcaba195175083a958ae7894
oleksandr.hnennyi <oleksandr.hnennyi@mev.com>
2022-06-22 01:52:02 +0300

    added signup and login routes

---
+++ src/views/LoginView.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="container">
+    <form @submit.prevent="login">
+      <div class="content">
+        <p class="login">Логін</p>
+        <div class="email">
+          <p class="label">Пошта</p>
+          <input type="text" name="email" v-model='email' placeholder="ivanko.ukraine@gmail.com" />
+        </div>
+        <div class="password">
+          <p class="label">Пароль</p>
+          <input type="text" name="password" v-model="password" placeholder="********" />
+        </div>
+        <button>Увійти</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style>
+.container {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.container:before:after {
+  box-sizing: inherit;
+}
+form {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+p {
+  margin: 0;
+  font-family: 'Gilroy-Light';
+}
+.content {
+  width: 56%;
+  display: flex;
+  flex-direction: column;
+  padding: 40px 40px 25px 40px;
+  box-sizing: border-box;
+}
+.login {
+  font-size: 32px;
+  line-height: 43.2px;
+}
+input {
+  width: 100%;
+  padding: 8px 16px;
+  font-size: 16px;
+  font-family: 'Gilroy-Light';
+  border: 2px solid #9ca3af;
+  border-radius: 8px;
+  margin-top: 4px;
+  color: #ffffff;
+}
+.email {
+  margin-top: 12px;
+}
+.label {
+  font-size: 16px;
+  line-height: 24px;
+}
+.password {
+  margin-top: 28px;
+}
+button {
+  font-size: 24px;
+  line-height: 43.2px;
+  background: #e5e7eb;
+  border: 4px solid #9ca3af;
+  border-radius: 8px;
+  margin-top: 35px;
+  padding: 4px;
+  color: #000000;
+}
+button:active {
+  transform: scale(0.98);
+  box-shadow: 3px 2px 3px 1px rgba(0, 0, 0, 0.24);
+}
+</style>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async login() {
+      const { email, password } = this;
+      const res = await fetch('http://localhost:3000/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      const data = await res.json();
+      console.log(res, data);
+      if (res.ok) {
+        this.$router.push('/');
+      }
+    },
+  },
+};
+</script>
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -2,16 +2,28 @@
   <div class="container">
     <form @submit.prevent="login">
       <div class="content">
-        <p class="login">Логін</p>
-        <div class="email">
-          <p class="label">Email</p>
-          <input type="text" name="email" v-model='email' placeholder="ivanko.ukraine@gmail.com" />
+        <p class="register">Реєстрація</p>
+        <div class="model-container">
+          <p class="label">Ім'я</p>
+          <input type="text" name="first_name" v-model="first_name" placeholder="..." />
         </div>
-        <div class="password">
+        <div class="model-container">
+          <p class="label">Прізвище</p>
+          <input type="text" name="last_name" v-model="last_name" placeholder="..." />
+        </div>
+        <div class="model-container">
+          <p class="label">Пошта</p>
+          <input type="text" name="email" v-model="email" placeholder="ivanko.ukraine@gmail.com" />
+        </div>
+        <div class="model-container">
           <p class="label">Пароль</p>
           <input type="text" name="password" v-model="password" placeholder="********" />
         </div>
-        <button>Увійти</button>
+        <select name="role" v-model="role">
+          <option>client</option>
+          <option>driver</option>
+        </select>
+        <button>Зареєструватися</button>
       </div>
     </form>
   </div>
@@ -46,43 +58,51 @@ p {
   padding: 40px 40px 25px 40px;
   box-sizing: border-box;
 }
-.login {
-  font-size: 36px;
+.register {
+  font-size: 32px;
   line-height: 43.2px;
 }
 input {
   width: 100%;
-  padding: 12px;
-  font-size: 20px;
+  padding: 8px 16px;
+  font-size: 16px;
   font-family: 'Gilroy-Light';
   border: 2px solid #9ca3af;
   border-radius: 8px;
   margin-top: 4px;
   color: #ffffff;
 }
-.email {
+.model-container {
   margin-top: 12px;
 }
 .label {
-  font-size: 20px;
+  font-size: 16px;
   line-height: 24px;
 }
 .password {
   margin-top: 28px;
 }
 button {
-  font-size: 36px;
+  font-size: 24px;
   line-height: 43.2px;
   background: #e5e7eb;
   border: 4px solid #9ca3af;
   border-radius: 8px;
   margin-top: 35px;
-  padding: 12px;
+  padding: 4px;
   color: #000000;
 }
 button:active {
   transform: scale(0.98);
   box-shadow: 3px 2px 3px 1px rgba(0, 0, 0, 0.24);
+}
+select {
+  border: 2px solid #9ca3af;
+  border-radius: 8px;
+  margin-top: 35px;
+  padding: 8px 16px;
+  font-size: 16px;
+  color: #ffffff;
 }
 </style>
 
@@ -91,25 +111,34 @@ export default {
   name: 'App',
   data() {
     return {
+      first_name: '',
+      last_name: '',
       email: '',
       password: '',
+      role: 'client',
     };
   },
   methods: {
     async login() {
-      const { email, password } = this;
-      const res = await fetch('http://localhost:3000/auth/login', {
+      const { first_name, last_name, email, password, role } = this;
+      const res = await fetch('http://localhost:3000/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
+          first_name,
+          last_name,
           email,
           password,
+          role,
         }),
       });
       const data = await res.json();
       console.log(data);
+      if (res.ok) {
+        this.$router.push('/');
+      }
     },
   },
 };
